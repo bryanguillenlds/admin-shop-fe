77400b6a97e9a09f3438be5bec4c2a7d2a044ba0
Bryan Guillén <bryanguillenlds@gmail.com>
2025-09-25 12:36:56 -0400

    Started layout for main view of the shop, usign tailwind component.

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <TopMenu />
+    <!-- Title -->
+    <div class="pt-32 bg-white">
+      <h1 class="text-center text-2xl font-bold text-gray-800">All Products</h1>
+    </div>
+
+    <!-- Main content area that grows to fill space -->
+    <main class="flex-1 bg-gray-100">
+      <RouterView />
+    </main>
+
+    <CustomFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TopMenu from '../components/TopMenu.vue'
+import CustomFooter from '../components/CustomFooter.vue'
+</script>
+
+<style scoped></style>
