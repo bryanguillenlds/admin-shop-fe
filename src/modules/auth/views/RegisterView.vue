c0982bc72859b42e84de22d51cc409de3f39d6d3
Bryan Guillén <bryanguillenlds@gmail.com>
2025-10-01 13:58:51 -0400

    Implemented actions and auth store for login and register, also updated components to perform those actions.

--- src/modules/auth/views/RegisterView.vue
+++ src/modules/auth/views/RegisterView.vue
@@ -1,55 +1,56 @@
 <template>
   <h1 class="text-2xl font-semibold mb-4">Register</h1>
-  <form action="#" method="POST">
+  <form @submit.prevent="onRegister">
     <!-- Username Input -->
     <div class="mb-4">
       <label for="name" class="block text-gray-600">Name</label>
       <input
+        v-model="registerForm.fullName"
         type="text"
         id="name"
         name="name"
         class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
         autocomplete="off"
+        ref="nameInput"
       />
     </div>
 
     <!-- Username Input -->
     <div class="mb-4">
-      <label for="username" class="block text-gray-600">Username</label>
+      <label for="email" class="block text-gray-600">Email</label>
       <input
+        v-model="registerForm.email"
         type="text"
-        id="username"
-        name="username"
+        id="email"
+        name="email"
         class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
         autocomplete="off"
+        ref="emailInput"
       />
     </div>
     <!-- Password Input -->
     <div class="mb-4">
       <label for="password" class="block text-gray-600">Password</label>
       <input
+        v-model="registerForm.password"
         type="password"
         id="password"
         name="password"
         class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
         autocomplete="off"
+        ref="passwordInput"
       />
-    </div>
-    <!-- Remember Me Checkbox -->
-    <div class="mb-4 flex items-center">
-      <input type="checkbox" id="remember" name="remember" class="text-blue-500" />
-      <label for="remember" class="text-gray-600 ml-2">Remember Me</label>
     </div>
     <!-- Forgot Password Link -->
     <div class="mb-6 text-blue-500">
       <a href="#" class="hover:underline">Forgot Password?</a>
     </div>
-    <!-- Login Button -->
+    <!-- Register Button -->
     <button
       type="submit"
       class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
     >
-      Login
+      Register
     </button>
   </form>
   <!-- Sign up  Link -->
@@ -57,3 +58,44 @@
     <RouterLink :to="{ name: 'login' }" class="hover:underline">Login Here</RouterLink>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { reactive, ref } from 'vue';
+import { useAuthStore } from '../stores/auth/auth.store';
+import { useToast } from 'vue-toastification';
+
+const authStore = useAuthStore();
+const toast = useToast();
+
+const nameInput = ref<HTMLInputElement | null>(null);
+const emailInput = ref<HTMLInputElement | null>(null);
+const passwordInput = ref<HTMLInputElement | null>(null);
+
+const registerForm = reactive({
+  fullName: '',
+  email: '',
+  password: '',
+});
+
+const onRegister = async () => {
+  if (registerForm.fullName === '') {
+    nameInput.value?.focus();
+  }
+
+  if (registerForm.email === '') {
+    emailInput.value?.focus();
+  }
+
+  if (registerForm.password.length < 6) {
+    passwordInput.value?.focus();
+  }
+
+  const resp = await authStore.register(registerForm.fullName, registerForm.email, registerForm.password);
+
+  if (resp) {
+    return;
+  }
+
+  toast.error('Failed to register');
+}
+</script>
